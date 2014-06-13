dd9b6bf367af7b50daeda9fc1330662100b24f1b
SvSchmidt <wqFrAABhMHRN4S1pk8lJ>
2014-06-13 14:53:11 +0200

    added possibility to upload own image (changed text again)

--- css3-filter-generator/js/dropzone.js
+++ css3-filter-generator/js/dropzone.js
@@ -309,7 +309,7 @@ require.register("dropzone/lib/dropzone.js", function (exports, module) {
       autoQueue: true,
       addRemoveLinks: false,
       previewsContainer: null,
-      dictDefaultMessage: "Drop image here to use an own image",
+      dictDefaultMessage: "Drop own image here to use it instead",
       dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
       dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
       dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
